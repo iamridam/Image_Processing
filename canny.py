3c552e9c533588f9048fa9e9f17a47d90a82eeae
iamridam <ridam101@gmail.com>
2018-08-17 01:55:34 +0530

    Canny edge detectio

---
+++ canny.py
@@ -0,0 +1,29 @@
+'''import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('ronaldo.jpg',0)
+edges = cv2.Canny(img,100,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()'''
+
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('new1.jpg',0)
+
+laplacian = cv2.Laplacian(img,cv2.CV_64F)
+
+plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+
+plt.show()
